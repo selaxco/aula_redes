6d82a8a861a6ceeba706d7af9bb012a5b87d386b
Thaynara Beatriz Selasco de Matos <thaynara220042@ilum.cnpem.br>
2023-03-09 17:19:08 -0300

    Adiciona o arquivo do Ex.A.2 resolvido e atualiza o arquivo de funções

--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -1,1 +1,8 @@
- 
+def funcao_objetivo_CB(individuo): #computa uma métrica do objetivo; nesse caso, é computado a soma dos valores de cada gene
+    '''computa a função objetivo no problema das caixas binárias
+    
+    Args: 
+        indivíduos: lista contendo os genes das caixas binárias
+        
+    Return valor que representa a soma dos genes de cada indivíduo '''
+    return sum(individuo) 
